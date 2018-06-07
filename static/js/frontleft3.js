863b7838fa2e84fdda5e9f8743c4b0d65a75a51c
Scott R Kaufman <scott@Scotts-MacBook-Pro.local>
2018-06-07 11:23:19 -0500

    Thursday morning push

--- static/js/frontleft3.js
+++ static/js/frontleft3.js
@@ -240,49 +240,49 @@ function renderMap(year, features, ramp, minVal, maxVal){
     output.innerHTML = slider.value; // Display the default slider value
 
 		// add a legend
-		var w = 140, h = 300;
+		// var w = 140, h = 300;
 
-		var key = svg
-			.append("g")
-			.attr("width", w)
-			.attr("height", h)
-			.attr("class", "legend");
+		// var key = svg
+		// 	.append("g")
+		// 	.attr("width", w)
+		// 	.attr("height", h)
+		// 	.attr("class", "legend");
 
-		var legend = key.append("defs")
-			.append("svg:linearGradient")
-			.attr("id", "gradient")
-			.attr("x1", "100%")
-			.attr("y1", "0%")
-			.attr("x2", "100%")
-			.attr("y2", "100%")
-			.attr("spreadMethod", "pad");
+		// var legend = key.append("defs")
+		// 	.append("svg:linearGradient")
+		// 	.attr("id", "gradient")
+		// 	.attr("x1", "100%")
+		// 	.attr("y1", "0%")
+		// 	.attr("x2", "100%")
+		// 	.attr("y2", "100%")
+		// 	.attr("spreadMethod", "pad");
 
-		legend.append("stop")
-			.attr("offset", "0%")
-			.attr("stop-color", highColor)
-			.attr("stop-opacity", 1);
+		// legend.append("stop")
+		// 	.attr("offset", "0%")
+		// 	.attr("stop-color", highColor)
+		// 	.attr("stop-opacity", 1);
 			
-		legend.append("stop")
-			.attr("offset", "100%")
-			.attr("stop-color", lowColor)
-			.attr("stop-opacity", 1);
+		// legend.append("stop")
+		// 	.attr("offset", "100%")
+		// 	.attr("stop-color", lowColor)
+		// 	.attr("stop-opacity", 1);
 
-		key.append("rect")
-			.attr("width", w - 100)
-			.attr("height", h)
-			.style("fill", "url(#gradient)")
-			.attr("transform", "translate(0,10)");
+		// key.append("rect")
+		// 	.attr("width", w - 100)
+		// 	.attr("height", h)
+		// 	.style("fill", "url(#gradient)")
+		// 	.attr("transform", "translate(0,10)");
 
-		var y = d3.scaleLinear()
-      .range([h, 0])
-      .domain([minVal, maxVal]);
+		// var y = d3.scaleLinear()
+    //   .range([h, 0])
+    //   .domain([minVal, maxVal]);
 
-		var yAxis = d3.axisRight(y);
+		// var yAxis = d3.axisRight(y);
 
-		key.append("g")
-			.attr("class", "y axis")
-			.attr("transform", "translate(41,10)")
-      .call(yAxis)
+		// key.append("g")
+		// 	.attr("class", "y axis")
+		// 	.attr("transform", "translate(41,10)")
+    //   .call(yAxis)
 
 
     //console.log(year);
